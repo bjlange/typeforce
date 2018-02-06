5a18fa13540ba7b87377a4a7bf05b31afa478726
Brian Lange <brian.lange@datascopeanalytics.com>
2018-02-06 09:48:50 -0600

    Initial experiment

--- app/main.js
+++ app/main.js
@@ -1,32 +1,45 @@
-
 import * as d3 from 'd3';
 
 // Local imports
-import { createCircleChart } from './circle';  // named import
-import makeBarChart from './bar';  // name is irrelevant since it is a default export
+// import { createCircleChart } from './circle';  // named import
+// import makeBarChart from './bar';  // name is irrelevant since it is a default export
 
-require('./main.scss');  // will build CSS from SASS 3 file
+require('./main.scss'); // will build CSS from SASS 3 file
 
-// eslint-disable-next-line no-unused-vars
-function buildCircleChart(chartWidth, chartHeight) {
-  // Dataset
-  const dataset = d3.range(20);
+// Main
+const vizDiv = document.getElementById('viz');
 
-  const svg = d3.select('#viz')
+const data = [];
+
+const svg = d3
+  .select(vizDiv)
                 .append('svg')
-                .attr('preserveAspectRatio', 'xMinYMin meet')
-                .attr('viewBox', `0 0 ${chartWidth} ${chartHeight}`)
-                .classed('svg-content-responsive', true);
+  .attr('width', 1000)
+  .attr('height', 2000);
 
-  // Test module, string interpolation
-  return createCircleChart(svg, dataset);
+let working_x = 500;
+let working_y = 15;
+const words = 'as pioneers of human-centered design, we keep people at the center of our work. it\'s a key tenet of design thinking, and even as our methods evolve in response to new, complex challenges, we’re always designing solutions for people first. we’re building to learn, and learning as we build, through inspiration, ideation, and implementation.';
+for (const word of words.split(' ')) {
+  working_x += Math.floor((Math.random() - 0.5) * 50);
+  working_y += 20;
+  data.push({ x: working_x, y: working_y, word });
 }
 
 // Main
 const vizDiv = document.getElementById("viz");
 
-const chartWidth = vizDiv.clientWidth;
-const chartHeight = vizDiv.clientHeight;
-// d3.select('#viz').remove();
-window.addEventListener('resize', makeBarChart('viz', d3.range(10), chartWidth, chartHeight));
-// buildCircleChart(chartWidth, chartHeight);
+svg
+  .selectAll('text')
+  .data(data)
+  .enter()
+  .append('text')
+  .attr('x', (d) => {
+    return d.x;
+  })
+  .attr('y', (d) => {
+    return d.y;
+  })
+  .text((d) => {
+    return d.word;
+  });
